e1672fb25544020cc14e86c9ae0e67e269f341a5
Matt <mmuhr88@gmail.com>
2018-07-09 21:01:39 -0700

    added the plus button, and when its clicked render buttons to add either a parent or a child

--- src/addButton.js
+++ src/addButton.js
@@ -1,4 +1,31 @@
 import React from "react"
-export function AddButton(familyMember) {
-  return <button> + </button>
+
+export class AddButton extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {isShowingMoreButtons: false}
+
+  }
+
+  render() {
+    if(this.state.isShowingMoreButtons){
+      return (
+        <React.Fragment>
+          <button>add parent</button>
+          <button>add child</button>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <button onClick={this.handlePlusClick}>
+          +
+        </button>
+      );
+    }
+  }
+
+  handlePlusClick = () => {
+    this.setState({isShowingMoreButtons: true})
+  }
 }
